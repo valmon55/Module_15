75662060db9c2bb0c5c2f41304966ecd38b7d7d6
valmon55 <fedor_ka@mail.ru>
2023-08-25 23:03:30 +0300

    final

---
+++ LINQ/LINQ.Final/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace LINQ.Final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+
+            Console.ReadKey();
+        }
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            return (from n in classes
+                    from s in n.Students
+                    orderby s
+                    select s).ToArray();
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
